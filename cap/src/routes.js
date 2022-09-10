abfca1f6d8810363b23180c6532e61c0645712d5
Gabriel Rodrigues <gsrodrigues2@ucs.br>
2022-09-10 20:42:38 -0300

    react routes

--- cap/src/routes.js
+++ cap/src/routes.js
@@ -1,16 +1,10 @@
 const path = require('path');
-// const controllersPath = path.join(__dirname, '/controllers/');
-// // const Controllers = requireDir('../src/controllers');
-// require('fs').readdirSync(controllersPath).forEach(function(file) {
-//     console.log(controllersPath + file);
-//     require(controllersPath + file);
-//   });
+
 const Prestadores = require('./controllers/Prestadores');
 
 module.exports = {
 
     async Action(controller, action, params) {
-        //console.log(controller + '.'+ action);
         return await eval(`${controller}.${action}`)(params).then((results)=>{
             return (results);
           });
